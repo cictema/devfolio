bc76dada3650d695e3742220b62a2ca71ffcb62d
Shubham Mazumder <shubham.mazumder@gmail.com>
2024-09-22 03:49:37 -0600

    add dialogflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,29 +19,33 @@ export default function RootLayout({
     <html lang="en">
       <head>
         
+      <link
+        rel="stylesheet"
+        href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css"
+      />
       </head>
       <body className="bg-gradient-to-tr from-blue-950 via-slate-950 to-neutral-950">
         <Navbar />
         {children}
         <Footer />
 
-        {/* Dialogflow widget */}
+        {/* Dialogflow Messenger Widget */}
         <df-messenger
           location="us-central1"
           project-id="movieassistant-436221"
           agent-id="71df484e-6e15-4587-8d4c-07bd70af50ef"
           language-code="en"
-          max-query-length="-1">
-          <df-messenger-chat-bubble
-          chat-title="MovieAssistant">
-          </df-messenger-chat-bubble>
+          max-query-length="-1"
+        >
+          <df-messenger-chat-bubble chat-title="Movie Assistant"></df-messenger-chat-bubble>
         </df-messenger>
+        {/* Scripts */}
         <Script
-        src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, df-messenger has been populated`)
-        }
+          src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"
+          strategy="afterInteractive"
+          onLoad={() => {
+            console.log('Dialogflow Messenger script loaded');
+          }}
         />
       </body>
     </html>
