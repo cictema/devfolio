038d87cbb86ef7be6795a64bb05abd2df726ae13
Shubham Mazumder <shubham.mazumder@gmail.com>
2024-09-22 04:43:13 -0600

    cors

--- next.config.js
+++ next.config.js
@@ -3,6 +3,31 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/v1/cx/locations/us-central1/integrations/messenger/webhook/projects/:projectId/agents/:agentId/sessions/:sessionId*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/dialogflow/:path*",
+        destination: "https://dialogflow.cloud.google.com/v1/cx/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
